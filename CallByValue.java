9162d994202987283bc97935df7dd5a0d9c84b09
pravallika-123-789 <grandhisilapravallika@gmail.com>
2022-02-07 17:38:44 +0530

    Practise

---
+++ CallByValue.java
@@ -0,0 +1,20 @@
+package callbyvalue;
+
+public class CallByValue {
+
+		int num=150;
+		
+		void operation(int num)
+		{
+			this.num=num*10/100;
+		}
+		public static void main(String[] args) {
+			CallByValue c=new CallByValue();
+			System.out.println("Value of num Before function call "+c.num);
+			
+			c.operation(100);
+			System.out.println("Value of num After function call "+c.num);
+		
+	}
+
+}
